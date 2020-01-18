9e9123b9f904a125bf07d19283d7da7678048e9b
Robert Zaremba <robert@zaremba.ch>
2020-01-18 22:32:36 +0100

    add comment to the test which should fail

--- test/2_token.test.js
+++ test/2_token.test.js
@@ -2,6 +2,7 @@ const Token = artifacts.require('Token');
 
 contract('CMTA20', function ([owner, address1, address2, address3, fakeRuleEngine]) {
 
+  // weird, this should fail, because the SafeMath library is not linked!
   it("should use add and SafeMath correctly", async function(){
     var t = await Token.deployed();
     var result = await t.add(2);
